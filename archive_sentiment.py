55202ff7e1b800f6b2f486cab0ebca1081d1237b
ryanraymartin <ryanraymartin@gmail.com>
2023-02-05 16:24:18 -0800

    added arg parser to archive sentiment

--- archive_sentiment.py
+++ archive_sentiment.py
@@ -1,51 +1,79 @@
+import argparse
+import os
+
 import pandas as pd
 import requests
 import time
 import dateutil
 import datetime
-import os
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-from typing import List
 from data_pipeline import *
 from tqdm import tqdm
 
-def send_request(date):
-    '''Sends a request to the NYT Archive API for given date.'''
-    base_url = 'https://api.nytimes.com/svc/archive/v1/'
-    url = base_url + '/' + date[0] + '/' + date[1] + '.json?api-key=' + os.environ["NYTIMES_API"]
-    response = requests.get(url).json()
-    time.sleep(6)
-    return response
+parser = argparse.ArgumentParser(description='Example of custom type usage')
+parser.add_argument('-start', '--start-datetime',
+                        dest='start_datetime',
+                        type=lambda d: datetime.datetime.strptime(d, '%Y-%m-%d').date(),
+                        default=None,
+                        required=True,
+                        help='start datetime in format "YYYY-MM-DD"')
+parser.add_argument('-stop', '--end-datetime',
+                        dest='stop_datetime',
+                        type=lambda d: datetime.datetime.strptime(d, '%Y-%m-%d').date(),
+                        default=datetime.date.today(),
+                        required=False,
+                        help='stop datetime in format "YYYY-MM-DD"')
 
-def is_valid(article, date):
-    '''An article is only worth checking if it is in range, and has a headline.'''
-    is_in_range = date > start and date < end
-    has_headline = type(article['headline']) == dict and 'main' in article['headline'].keys()
-    return is_in_range and has_headline
+class NYTimesDataModel: 
+    """A data model to retrieve data from nytimes archive and label with sentiment scores"""
 
-def parse_response(response):
-    '''Parses and returns response as pandas data frame.'''
-    data = {'headline': [], 'date': []}
-    articles = response['response']['docs'] 
-    for article in articles: # For each article, make sure it falls within our date range
-        date = dateutil.parser.parse(article['pub_date']).date()
-        # Nov. 12 change from "Business Day" to just "Business"
-        if is_valid(article, date) and 'Business' in article['section_name']:
-            data['date'].append(date)
-            data['headline'].append(article['headline']['main']) 
-    return pd.DataFrame(data)
+    def __init__(self, start, stop) -> pd.DataFrame:
+        self.dp = DataPipeline()
+        self.start = start
+        self.stop = stop
+    
+    def send_request(self, date):
+        '''Sends a request to the NYT Archive API for given date.'''
+        base_url = 'https://api.nytimes.com/svc/archive/v1/'
+        url = base_url + '/' + date[0] + '/' + date[1] + '.json?api-key=' + os.environ["NYTIMES_API"]
+        response = requests.get(url).json()
+        time.sleep(6)
+        return response
+
+    def is_valid(self, article, date):
+        '''An article is only worth checking if it is in range, and has a headline.'''
+        is_in_range = date > self.start and date < self.stop
+        has_headline = type(article['headline']) == dict and 'main' in article['headline'].keys()
+        return is_in_range and has_headline
+
+    def parse_response(self, response):
+        '''Parses and returns response as pandas data frame.'''
+        data = {'headline': [], 'date': []}
+        articles = response['response']['docs'] 
+        for article in articles: # For each article, make sure it falls within our date range
+            date = dateutil.parser.parse(article['pub_date']).date()
+            # Nov. 12 change from "Business Day" to just "Business"
+            if self.is_valid(article, date) and 'Business' in article['section_name']:
+                data['date'].append(date)
+                data['headline'].append(article['headline']['main']) 
+        return pd.DataFrame(data)
+
+    def extract(self) -> pd.DataFrame:
+        months_in_range = [x.split(' ') for x in pd.date_range(self.start, self.stop, freq='MS').strftime("%Y %-m").tolist()]
+        month_object = tqdm(months_in_range)
+        for date in month_object:
+            month_object.set_description(f"Month in progress: {date}")
+            response = self.send_request(date)
+            data = self.parse_response(response)
+            df = self.dp.transform(data)
+            self.dp.load(df)
+
 
 if __name__ == '__main__':
-    dp = DataPipeline()
-    end = datetime.date.today()
-    start = datetime.date(2022, 10, 1)
-    months_in_range = [x.split(' ') for x in pd.date_range(start, end, freq='MS').strftime("%Y %-m").tolist()]
-    month_object = tqdm(months_in_range)
-    for date in month_object:
-        month_object.set_description(f"Month in progress: {date}")
-        response = send_request(date)
-        data = parse_response(response)
-        df = dp.transform(data)
-        #df.to_csv('nytimes_sentiment/{}_{}'.format(date[0], date[1]))
-        dp.load(df)
+    args = parser.parse_args()
+    start_date = args.start_datetime
+    stop_date = args.stop_datetime
+    NYTimesDataModel(start=start_date, stop=stop_date).extract()
+
+    
