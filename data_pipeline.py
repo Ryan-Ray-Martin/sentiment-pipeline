fdb03f10356e66a7d9d8c217fbc1bce1e34a5a3a
ryanraymartin <ryanraymartin@gmail.com>
2023-02-17 12:04:55 -0800

    select method for queries

--- data_pipeline.py
+++ data_pipeline.py
@@ -2,13 +2,19 @@ import requests
 import sqlalchemy
 from cloud_sql_tcp import connect_tcp_socket
 from tqdm.auto import tqdm
+import pandas as pd
 
 class DataPipeline:
     def __init__(self) -> None:
-        pass
+        self.get_data = """SELECT * FROM nytimes"""
+        self.get_avg = """SELECT date, AVG(headline_sentiment_positive) as positive
+                          FROM nytimes
+                          GROUP BY date
+                          ORDER BY date ASC"""
 
     def query(self, payload):
-        return requests.get("http://rayservice-sample-serve-svc:8000/", params={"text": str(payload)}).json()
+        # "http://rayservice-sample-serve-svc:8000/"
+        return requests.get("http://localhost:8000/", params={"text": str(payload)}).json()
 
     def transform(self, df: dict) -> dict:
         sentiments = ["positive", "negative", "neutral"]
@@ -28,3 +34,7 @@ class DataPipeline:
     def load(self, df: dict)-> sqlalchemy.engine.base.Engine:
         pool = connect_tcp_socket()
         df.to_sql('nytimes', con=pool, if_exists='append', index=False)
+
+    def select(self)-> sqlalchemy.engine.base.Engine:
+        pool = connect_tcp_socket()
+        return pd.read_sql(self.get_avg, con=pool)
